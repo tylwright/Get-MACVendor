8ece53fc08c216bbfc2398f42565da7614a6fde9
Tyler Wright <tylwright@gmail.com>
2015-09-16 23:24:32 -0400

    Added ability to update the OUI list.  Various bug fixes.

--- README.md
+++ README.md
@@ -6,11 +6,19 @@ This PowerShell script searches the IEEE's database of vendors/MAC addresses to 
 ## How to use
 ```
 .\getMACVendor.ps1 -MAC 00:12:23
+.\getMACVendor.ps1 -MAC 00:12:23:45:54:32
 ```
 The script will output: *Pixim*
 
+## Updating the OUI list
+```
+.\getMACVendor.ps1 -Action Update
+```
+The script will pull a fresh version of the OUI list from <a href='http://standards-oui.ieee.org/oui.txt'>http://standards-oui.ieee.org/oui.txt</a>.
+
 ## Requirements
-In order to use this script, please keep the file: *vendors.txt* in the same folder as the script.  This file contains the relationships between certain MAC addresses and vendors.  An up-to-date list can be obtained from here: <a href='http://standards-oui.ieee.org/oui.txt'>http://standards-oui.ieee.org/oui.txt</a>.
+In order to use this script, please keep the file: *vendors.txt* in the same folder as the script.  This file contains the relationships between certain MAC addresses and vendors.
 
 ## Changelog
 [August 3, 2015] [v0.1] - Script created
+[September 16, 2015] [v0.2] - Added ability to update the OUI list.  Various bug fixes.
--- getMACVendor.ps1
+++ getMACVendor.ps1
@@ -1,61 +1,172 @@
-﻿[CmdletBinding()]
-Param(
-    [Parameter(Mandatory=$True)]
-    [string]$MAC
-
-    #[Parameter(Mandatory=$False)]
-    #[String]$vendorList
+﻿#region Script Arguments
+[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory = $false,
+			   Position = 1,
+			   HelpMessage = 'MAC Address in any format')]
+	[ValidateNotNullOrEmpty()]
+	[string]$MAC,
+	[ValidateSet('update', IgnoreCase = $true)]
+	[string]$action
 )
+#endregion
+
+#region Variables
+# Set debug preference
+$DebugPreference = "Continue"
 
 # Default location for vendor list
 $vendorList = "$pwd/vendor.txt"
+#endregion
 
-# Clean MAC
-function Clean-MAC{
-    # Change : to -
-    $MAC = $MAC -replace ":","-"
-    
-    # Only use the first 00:00:00 character
-    if($MAC.length -lt 7){
-        $MAC = $MAC.Substring(0, 8)
-    }
-    return $MAC
+#region Functions
+<#
+	.SYNOPSIS
+		Updates vendor/MAC list
+	
+	.DESCRIPTION
+		Retrieves a list of vendors and their assigned MAC addresses from  standards-oui.ieee.org/oui.txt.
+#>
+function Update-VendorList
+{
+	Try
+	{
+		# If a vendor list already exists, rename it as "old"
+		if (Test-Path $vendorList)
+		{
+			# In the event that an old copy of the OUI list was not deleted, delete it now before a new backup is made
+			if (Test-Path "$vendorList.old")
+			{
+				Remove-Item -Path "$vendorList.old"
+			}
+			Rename-Item -Path $vendorList "$vendorList.old"
+		}
+		# Download vendor list
+		Write-Debug "Attempting to download a new OUI list"
+		$webclient = New-Object System.Net.WebClient
+		$url = "http://standards-oui.ieee.org/oui.txt"
+		$webclient.DownloadFile($url, $vendorList)
+		# If vendor list downloaded successfully, delete the "old" copy
+		if (Test-Path $vendorList)
+		{
+			Write-Debug "Vendor list has been updated - deleting older version"
+			Remove-Item -Path "$vendorList.old"
+		}
+	}
+	Catch
+	{
+		Write-Debug "Unable to download the latest OUI list from http://standards-oui.ieee.org"
+		Write-Debug "Reverting to previous OUI list"
+		# Rename the "old" copy back to the default name.
+		# We want to be able to use this script even if our update function fails.
+		Rename-Item "$vendorList.old" $vendorList
+	}
 }
 
-# Get updated list from IEEE
-#function Get-VendorList{
-    #$client = New-Object System.Net.WebClient
-    #$url = "http://standards-oui.ieee.org/oui.txt"
-    #$storeAs = "$pwd/vendor.txt"
-    #$client.DownloadFile($url, $storeAs)
-#}
-
-# Search vendor list for match
-function Get-Vendor{
-    [cmdletbinding()]
-    Param(
-        [string]$MAC
-    )
-    Process{
-        Try{
-            $output = Select-String -Path "$pwd/vendor.txt" -pattern $MAC
-            $output = $output -replace ".*(hex)"
-            $output = $output.Substring(3)
-            return $output
-        }
-        Catch{
-            "MAC address was not found"
-        }
-    }
+<#
+	.SYNOPSIS
+		Makes sure that the given MAC is formatted correctly
+	
+	.DESCRIPTION
+		First, the function removes any ":" or "-" characters from the MAC address.
+		Second, the function limits the number of characters down to the first eight characters (vendors are only assigned the first three sections in a MAC address).
+	
+	.PARAMETER MAC
+		Provide a MAC address in string format
+#>
+function Clean-MAC
+{
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'Provide a MAC address in string format')]
+		[ValidateNotNullOrEmpty()]
+		[string]$MAC
+	)
+	
+	# Change : to -
+	$MAC = $MAC -replace ":", "-"
+	
+	# Only use the first 00:00:00 character
+	if ($MAC.length -gt 7)
+	{
+		$MAC = $MAC.Substring(0, 8)
+	}
+	return $MAC
 }
 
-function Get-MAC{
-    $MAC = Get-WmiObject win32_networkadapterconfiguration | SELECT macaddress
-    $MAC = $MAC.macaddress
-    foreach($address in $MAC){
-        Get-Vendor(Clean-MAC($address))
-    }
+<#
+	.SYNOPSIS
+		Finds the vendor of a given MAC address
+	
+	.DESCRIPTION
+		Searches for the given MAC address in the OUI list and returns the name of the vendor
+	
+	.PARAMETER MAC
+		Provide a MAC address that has undergone the Clean-MAC function
+#>
+function Get-Vendor
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'Provide a MAC address that has undergone the Clean-MAC function')]
+		[ValidateLength(8, 8)]
+		[ValidateNotNullOrEmpty()]
+		[string]$MAC
+	)
+	
+	Process
+	{
+		Try
+		{
+			$output = Select-String -Path $vendorList -pattern $MAC
+			$output = $output -replace ".*(hex)"
+			$output = $output.Substring(3)
+			return $output
+		}
+		Catch
+		{
+			Write-Warning "MAC address was not found"
+		}
+	}
 }
+#endregion
 
-# Main
-Get-Vendor(Clean-MAC($MAC))
+#region Main Program
+# If the user wants to update the vendor list, direct them away from the vendor search
+if ($action.ToLower() -eq "update")
+{
+	Update-VendorList
+	Exit
+}
+else
+{
+	# Clean and format the given MAC address
+	$cleanedMAC = Clean-MAC -MAC $MAC
+	
+	# Get the vendor of the MAC address
+	$vendor = Get-Vendor -MAC $cleanedMAC
+	
+	# If there is a vendor, output the name
+	if ($vendor)
+	{
+		# If the vendor name ends with a period (ex. inc.), don't print an additional "."
+		if (($vendor.Substring($vendor.Length - 1), 1) -eq ".")
+		{
+			Write-Output "The vendor is: $vendor"
+		}
+		else
+		{
+			Write-Output "The vendor is: $vendor."
+		}
+	}
+	else
+	{
+		Write-Output "Unable to find the vendor of $MAC."
+	}
+	Exit
+}
+#endregion
